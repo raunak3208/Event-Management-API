5fb6a1788d7f287733c40349116926c125a8ee2d
Raunak Kumar Jha <raunak3208@users.noreply.github.com>
2026-03-14 23:05:50 +0530

    Feat: Delete Event

--- controller/event-controller.js
+++ controller/event-controller.js
@@ -99,10 +99,38 @@ const updateEvent=async (req,res)=>{
         })
     }
 }
+
+const deleteEvent=async (req,res)=>{
+    try {
+        const {id}=req.params;
+        const currentUser=req.user;
+        const deletedEvent=await eventService.deleteEvent(id,currentUser);
+        if(!deletedEvent){
+            return res.status(403).json({
+                message: 'Sorry, you are not an organizer and cannot delete this event.',
+              });
+        }
+        res.status(200).json({
+            message:"Event deleted successfully",
+            data:deletedEvent,
+            success:true,
+            err:{}
+        })
+    } catch (error) {
+        console.log("Error in event controller",error);
+        res.status(500).json({
+            message:"Internal server error",
+            err:error,
+            data:{},
+            success:false
+        })
+    }
+}
 export {
     createEvent,
     getEvents,
     getEvent,
-    updateEvent
+    updateEvent,
+    deleteEvent
     
 }
