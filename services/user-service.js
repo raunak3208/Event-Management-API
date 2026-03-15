fd0aec50cb07c559624560963d2bf3decfd57c87
Raunak Kumar Jha <raunak3208@users.noreply.github.com>
2026-03-15 20:21:49 +0530

    Feat: user-service functionalities

--- services/user-service.js
+++ services/user-service.js
@@ -2,69 +2,77 @@ import UserRepository from '../repository/user-repository.js';
 import { checkUserAccess } from '../utils/authUtils.js';
 
 class UserService {
-    constructor() {
-        this.userRepository = new UserRepository();
+  constructor() {
+    this.userRepository = new UserRepository();
+  }
+
+  async updateUser(id, data) {
+    try {
+      const user = await this.userRepository.updateUser(id, data);
+      return user;
+    } catch (error) {
+      console.log('Something went wrong in user-service ', error);
+      throw error;
     }
+  }
 
-    async updateUser(id, data) {
-        try {
-            const user = await this.userRepository.updateUser(id, data);
-            return user;
-        } catch (error) {
-            console.log('Something went wrong in user-service ', error);
-            throw error;
-        }
+  async getUser(currentUser, id) {
+    try {
+      console.log('currentUser', currentUser.role);
+      if (!checkUserAccess(currentUser.id, id, currentUser.role)) {
+        throw new Error('Access denied: You can only view your own data or if you are an admin.');
+      }
+
+      const response = await this.userRepository.getUser(id);
+      return response;
+    } catch (error) {
+      console.log('Something went wrong in user-service ', error);
+      throw error;
     }
+  }
 
-    async getUser(currentUser, id) {
-        try {
-            console.log('currentUser', currentUser.role);
-            if (!checkUserAccess(currentUser.id, id, currentUser.role)) {
-                throw new Error('Access denied: You can only view your own data or if you are an admin.');
-            }
+  async getAllUsers(query, page, limit) {
+    try {
+      const skip = (page - 1) * limit;
 
-            const response = await this.userRepository.getUser(id);
-            return response;
-        } catch (error) {
-            console.log('Something went wrong in user-service ', error);
-            throw error;
-        }
+      const totalUsers = await this.userRepository.countUsers(query);
+
+      const users = await this.userRepository.findAllUsers(query, skip, limit);
+
+      const totalPages = Math.ceil(totalUsers / limit);
+
+      return {
+        users,
+        totalUsers,
+        currentPage: page,
+        totalPages
+      };
+    } catch (error) {
+      console.error("Error in UserService while fetching users:", error);
+      throw error;
     }
+  }
 
-    async getAllUsers(query, page, limit) {
-        try {
-            const skip = (page - 1) * limit;
 
-            const totalUsers = await this.userRepository.countUsers(query);
-
-            const users = await this.userRepository.findAllUsers(query, skip, limit);
-
-            const totalPages = Math.ceil(totalUsers / limit);
-
-            return {
-                users,
-                totalUsers,
-                currentPage: page,
-                totalPages
-            };
-        } catch (error) {
-            console.error("Error in UserService while fetching users:", error);
-            throw error;
-        }
+  async deleteUser(id) {
+    try {
+      const user = await this.userRepository.deleteUser(id);
+      return user;
+    } catch (error) {
+      console.error("Error in UserService while soft deleting user:", error);
+      throw error;
     }
+  }
 
-
-    async deleteUser(id) {
-        try {
-            const user = await this.userRepository.deleteUser(id);
-            return user;
-        } catch (error) {
-            console.error("Error in UserService while soft deleting user:", error);
-            throw error;
-        }
+  async findUser(id) {
+    try {
+      const response = await this.userRepository.getUser(id);
+      return response;
+    } catch (error) {
+      console.error("Error in UserService finding user", error);
+      throw error;
     }
-
-
+  }
 }
 
 export default UserService;
