3bc110c54cadf20c7a3a9deccd4b61c5ce03670b
Raunak Kumar Jha <raunak3208@users.noreply.github.com>
2026-03-20 20:15:35 +0530

    configuration for server

---
+++ config/server-config.js
@@ -0,0 +1,8 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const PORT = process.env.PORT;
+export const MONGODB_URI = process.env.MONGODB_URI;
+export const JWT_SECRET = process.env.JWT_SECRET;
+export const APP_PASS=process.env.APP_PASS;
+export const SENDER_EMAIL=process.env.SENDER_EMAIL;
